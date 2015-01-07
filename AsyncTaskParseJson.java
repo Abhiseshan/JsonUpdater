bd2230085cca7bf96277d6292defa8421ffb1154
Abhiseshan <abhinavseshan@hotmail.com>
2015-01-06 23:26:49 -0500

    Update AsyncTaskParseJson.java

--- AsyncTaskParseJson.java
+++ AsyncTaskParseJson.java
@@ -1,4 +1,3 @@
-package com.esp.screwesp;
 
 import android.app.Fragment;
 import android.content.Context;
@@ -8,7 +7,7 @@ import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 
-import com.pkmmte.view.CircularImageView;
+import android.widget.ImageView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -20,22 +19,23 @@ import org.json.JSONObject;
 
 public class AsyncTaskParseJson extends AsyncTask<String, String, String> {
 
-    private Context mContext;
-    private View rootView;
-    private CircularImageView cView;
+    private ImageView iView;
 
-    public AsyncTaskParseJson(CircularImageView circularImageView){
+    public AsyncTaskParseJson(ImageView imageView){
         //this.mContext = context;
         //this.rootView = rootView;
-        this.cView = circularImageView;
+        this.iView = imageView;
 
     }
 
-    static String version = "1.0.0";
+    //Default value for server version
+    static String server_version = "1.0.0";
+    
     final String TAG = "AsyncTaskParseJson.java";
 
-    // set your json string url here
-    String Url = "http://techfest13.co.nf/version_control.json";
+    // URL to get version_control JSON - replace with web address
+    String url = "http://x.com/version_control.json";
+
 
     // contacts JSONArray
     JSONArray dataJsonArr = null;
@@ -55,7 +55,7 @@ public class AsyncTaskParseJson extends AsyncTask<String, String, String> {
             JSONObject json = jParser.getJSONFromUrl(Url);
 
             // get the array of users
-            dataJsonArr = json.getJSONArray("Users");
+            dataJsonArr = json.getJSONArray("Version");
 
             // loop through all users
             for (int i = 0; i < dataJsonArr.length(); i++) {
@@ -63,11 +63,10 @@ public class AsyncTaskParseJson extends AsyncTask<String, String, String> {
                 JSONObject c = dataJsonArr.getJSONObject(i);
 
                 // Storing each json item in variable
-                version = c.getString("version");
-                //version = version.toString();
+                server_version = c.getString("current");
 
                 // show the values in our logcat
-                Log.e(TAG, "version: " + version);
+                Log.e(TAG, "version: " + server_version);
 
             }
 
@@ -81,19 +80,18 @@ public class AsyncTaskParseJson extends AsyncTask<String, String, String> {
     @Override
     protected void onPostExecute(String strFromDoInBg) {
 
+        //Local app version
         String current_version = "1.0.0";
-        String server_version = AsyncTaskParseJson.return_version();
+        
+        //Compare version - Local version and server version
         boolean result = current_version.equals(server_version);
+        
         Log.d("Result", "Result = " + result + " server version= " + server_version + " local version =" + current_version);
 
+        //If the versions dont match, put your code to notify user about the update
+        //In my app this shows an image
         if (!result) {
-            //CircularImageView circularImageView = (CircularImageView) rootView.findViewById(R.id.circularview);
-            cView.setImageResource(R.drawable.update);
+            iView.setImageResource(R.drawable.update);
         }
-    }
-
-    public static String return_version()
-    {
-        return version;
     }
 }
