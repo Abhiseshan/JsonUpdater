2f65912e89d75b714b8e78ef8ec79602bf029717
Abhiseshan <abhinavseshan@hotmail.com>
2015-01-06 23:27:33 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,23 +1,22 @@
+
+import android.widget.ImageView;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
 public class MainActivity extends Activity {
 
     //Json Node names
     private static final String TAG_VERSION = "current";
-
-    // URL to get version_control JSON - replace with web address
-    private static String url = "http://x.com/version_control.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main;
         
-        LayoutInflater inflater = (LayoutInflater)context.getSystemService
-                (Context.LAYOUT_INFLATER_SERVICE);
 
-        View myFragmentView = inflater.inflate(R.layout.activity_main, null);
-
-        Context cont = MainActivity.this;
-        
         //This is the image view of the image you want to notify the user with when an upadate is avaliable
         final ImageView imageView = (ImageView)findViewById(R.id.imageview);
         
